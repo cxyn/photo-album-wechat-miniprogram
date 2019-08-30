b0a7ea0d724e03bd35f5cc4bec138b8ac657e70c
colin <i@wangyunxiao.com>
2019-08-30 22:51:30 +0800

    引入Taro

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,28 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+export default class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '多多的成长记录'
+  }
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Hello 多多!</Text>
+      </View>
+    )
+  }
+}
